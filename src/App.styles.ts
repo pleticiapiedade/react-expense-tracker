cca37dabfb27d426a60b55bfd89b57852dace129
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-09 18:23:42 -0300

    :sparkles: Styled components

---
+++ src/App.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const Header = styled.div`
+    background-color: #E5E5E5;
+    height: 12rem;
+    text-align: center;
+`;
+
+export const HTitle = styled.h1`
+    margin: 0;
+    padding: 0;
+    padding-top: 2rem;
+    color: #868698;
+    font-weight: 500;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    margin-bottom: 5rem;
+    max-width: 1316px;
+    width: 100%;
+    
+`;
