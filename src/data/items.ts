08e7bdf8a2b1ad4aad70afe78c7acac2d92a5687
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-09 21:54:46 -0300

    :construction: list filters

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,9 +1,9 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    {date: new Date(2021, 13, 3), category: 'food', title:'Bobs Burguer', value: 43.30 },
-    {date: new Date(2021, 13, 3), category: 'shopping', title:'Roupas Novas', value: 180.40 },
-    {date: new Date(2021, 13, 9), category: 'food', title:'Pizzaria', value: 92.35 },
-    {date: new Date(2021, 13, 10), category: 'rent', title:'Aluguel Apt', value: 2000 },
-    {date: new Date(2021, 12, 9), category: 'salary', title:'Salário', value: 5000 }
+    {date: new Date(2022, 0, 3), category: 'food', title:'Bobs Burguer', value: 43.30 },
+    {date: new Date(2022, 0, 3), category: 'shopping', title:'Roupas Novas', value: 180.40 },
+    {date: new Date(2022, 0, 9), category: 'food', title:'Pizzaria', value: 92.35 },
+    {date: new Date(2022, 0, 10), category: 'rent', title:'Aluguel Apt', value: 2000 },
+    {date: new Date(2021, 13, 5), category: 'salary', title:'Salário', value: 5000 }
 ];
