35f8fc5cd37c7992504a4d8e5c02618dfc73617e
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-09 22:43:32 -0300

    :construction: Date formating

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,5 +1,6 @@
 import * as C from './styles'
 import { Item } from '../../types/Item'
+import { formatDate } from '../../helpers/dateFilter'
 
 type Props = {
     item: Item;
@@ -9,7 +10,7 @@ export const TableItem = ({item}: Props) => {
     return(
 
         <C.TableLine>
-            <C.TableColumn>...</C.TableColumn>
+            <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
             <C.TableColumn>{item.category}</C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
             <C.TableColumn>R$ {item.value}</C.TableColumn>
