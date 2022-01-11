bf89cb1b58eafe825ea32737644c28bbf391e7ab
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-10 21:21:17 -0300

    :lipstick: categories and value colors

--- src/components/TableItem/styles.ts
+++ src/components/TableItem/styles.ts
@@ -7,3 +7,16 @@ export const TableLine = styled.tr`
 export const TableColumn = styled.td`
     padding: 10px 0;
 `;
+
+export const Category = styled.div<{color: string}>`
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: #FFFF;
+    background: ${props => props.color};
+`;
+
+export const Value = styled.div<{color: string}>`
+    color: ${props => props.color}
+
+`;
