63358623c130c8891a160bc7bbea844c72c5be68
Leticia Piedade <pleticiapiedade@gmail.com>
2022-01-11 22:59:35 -0300

    :sparkles: Calc and show expenses, incomes and balance

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,29 @@ const App = () => {
   const[list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
-
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
 
+  useEffect(()=> {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredList){
+      if(categories[filteredList[i].category].expense){
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
@@ -32,6 +49,12 @@ const App = () => {
       </C.Header>
       <C.Body>
         
+        <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMonthChange}
+        income={income}
+        expense={expense}
+        />
 
         {/*Area de inserir Informacoes*/}
 
